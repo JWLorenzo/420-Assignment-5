76979dacc587caf10a41bb1cbfc016503b5bfe08
JWLorenzo <jacob.w.lorenzo@maine.edu>
2022-04-24 19:31:18 -0400

    Update FinalTest.py

--- FinalTest.py
+++ FinalTest.py
@@ -55,30 +55,48 @@ def VerifyCredit(text):
 class TestValidity(unittest.TestCase):
   def test_FirstIsntChar(self):
     self.assertEqual(FirstIsntChar("abcdef7@ijklmnop"),"1")
+  def test_FirstIsntChar2(self):
+    self.assertEqual(FirstIsntChar("^bcdef7@ijklmnop"),"0")
 #################################################################
   def test_FirstIsntDigit(self):
     self.assertEqual(FirstIsntDigit("abcdef7@ijklmnop"),"1")
+  def test_FirstIsntDigit2(self):
+    self.assertEqual(FirstIsntDigit("5bcdef7@ijklmnop"),"0")
 #################################################################
   def test_ContainsChar(self):
     self.assertEqual(ContainsChar("abcdef7@ijklmnop"),"1")
+  def test_ContainsChar2(self):
+    self.assertEqual(ContainsChar("abcdef7hijklmnop"),"0")
 #################################################################
   def test_ContainsLetter(self):
     self.assertEqual(ContainsLetter("abcdef7@ijklmnop"),"1")
+  def test_ContainsLetter2(self):
+    self.assertEqual(ContainsLetter("1111111111111111"),"0")
 #################################################################
   def test_ContainsDigit(self):
     self.assertEqual(ContainsDigit("abcdef7@ijklmnop"),"1")
+  def test_ContainsDigit2(self):
+    self.assertEqual(ContainsDigit("abcdefg@ijklmnop"),"0")
 #################################################################
   def test_CheckLength16(self):
     self.assertEqual(CheckLength16("abcdef7@ijklmnop"),"1")
+  def test_CheckLength162(self):
+    self.assertEqual(CheckLength16("abcdef7@ijklmnopq"),"0")
 #################################################################
   def test_OnlyNumCheck(self):
     self.assertEqual(OnlyNumCheck("1234567891234567"),"1")
+  def test_OnlyNumCheck2(self):
+    self.assertEqual(OnlyNumCheck("a234567891234567"),"0")
 #################################################################
   def test_VerifyPassword(self):
     self.assertEqual(VerifyPassword("abcdef7@ijklmnop"),"111111")
+  def test_VerifyPassword2(self):
+    self.assertEqual(VerifyPassword("%bcdef7@ijklmnop"),"111110")
 #################################################################
   def test_VerifyCredit(self):
     self.assertEqual(VerifyCredit("1234567891234567"),"11")
+  def test_VerifyCredit2(self):
+    self.assertEqual(VerifyCredit("a234567891234567"),"10")
 #################################################################
 if __name__ == "__main__":
     unittest.main()
